2bd5d29d15e1e3f0e5bfacba3a8eff189df756ef
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-01-10 23:37:14 +0530

    Email sending feature implemented

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Keep these routes for some uses of the System Administrator
 router
