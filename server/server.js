1135014421a6245cb1929170e6fea30a449c4863
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-01-10 06:33:05 +0530

    Uncaught exception errors were handled in synchronous code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION...Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -24,8 +30,8 @@ const server = app.listen(PORT, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION...Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
