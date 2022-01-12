d912ff1de038086c5dcd73fcda0fb43a60125bbe
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-01-12 22:25:07 +0530

    multer installed. final commit before file management

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -69,6 +69,17 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    // loggedout is just a dummy text. we send it instead of the token
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    status: 'success',
+  });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
@@ -77,11 +88,9 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
-
-  // } else if (req.cookies.jwt) {
-  //   token = req.cookies.jwt;
-  // }
 
   if (!token) {
     return next(
@@ -116,6 +125,37 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+// Only for rendered pages, no errors
+// exports.isLoggedIn = catchAsync(async (req, res, next) => {
+//   if (req.cookies.jwt) {
+//     try {
+//       // 1) Verify token
+//       const decoded = await promisify(jwt.verify)(
+//         req.cookies.jwt,
+//         process.env.JWT_SECRET
+//       );
+
+//       // 2) Check if user still exists
+//       const currentUser = await User.findById(decoded.id);
+//       if (!currentUser) {
+//         return next();
+//       }
+
+//       // 3) Check if user changed password after the token was issued
+//       if (currentUser.changedPasswordAfter(decoded.iat)) {
+//         return next();
+//       }
+
+//       //  THERE IS A LOGGED IN USER
+//       res.locals.user = currentUser;
+//       return next();
+//     } catch (err) {
+//       return next();
+//     }
+//   }
+//   next();
+// });
 
 exports.restrictTo =
   (...roles) =>
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 // const factory = require('./handlerFactory');
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
