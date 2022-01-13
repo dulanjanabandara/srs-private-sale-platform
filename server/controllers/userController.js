51159854af158db2e762cab7b21443a3006e83e9
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-01-13 09:12:21 +0530

    User profile resizing feature implenented

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,23 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // cb - callback, cb is not coming from next, different from next()
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    // user-userId-currentTimestamp.jpeg -> user-hakrfh853jalr0-22082022.jpeg
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // cb - callback, cb is not coming from next, different from next()
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // user-userId-currentTimestamp.jpeg -> user-hakrfh853jalr0-22082022.jpeg
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -30,6 +33,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('profilePhoto');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
